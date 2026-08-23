ef444bfbecde6c990342fcd66493d56bad8e67f8
Marzzzo <marco.burdick@web.de>
2026-08-23 11:25:47 +0200

    added rendering the startscreen via a template

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,26 @@
+//base
 import "../scss/main.scss";
+import "../scss/base/_fonts.scss";
+import "../scss/abstract/_mixin.scss";
+
+//components
+import "../scss/components/_buttons.scss";
+
+//pages
+import "../scss/pages/_start-page.scss";
+import "../scss/pages/_setting-page.scss";
+
+//templates
+import { startPage } from "./templates/start-page";
+
+function init(): void {
+  showStartPage();
+}
+
+const app = document.getElementById("app")!;
+
+function showStartPage(): void {
+  app.innerHTML = startPage();
+}
+
+init();
